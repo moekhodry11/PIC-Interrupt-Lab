c66e2faf20315ec693968a025f1c0465a4c12618
moekhodry11 <86708003+moekhodry11@users.noreply.github.com>
2023-05-18 13:23:14 +0300

    just a commit

--- Code/Interrupts_Lab.c
+++ Code/Interrupts_Lab.c
@@ -1,11 +1,12 @@
 unsigned short kp;
-char key;
-char password[20];
-char My_pass[4] = {'1','2','3','4'};
-int count =0, flag = 1,i=0;
+char user_password[20];
+char My_pass[] = "1234";
+char txt[7];
 
+int counter = 0, pass_index = 0, Star_index = 1 ,motor_running= 0,time=0;
+ 
 // Keypad module connections
-char  keypadPort at PORTD;
+char keypadPort at PORTD;
 
 // LCD module connections
 sbit LCD_RS at RC4_bit;
@@ -22,108 +23,130 @@ sbit LCD_D5_Direction at TRISC1_bit;
 sbit LCD_D6_Direction at TRISC2_bit;
 sbit LCD_D7_Direction at TRISC3_bit;
 
-//Motor
+// Motor
 sbit Motor_Pin at RB7_bit;
 sbit Motor_Direction at TRISB7_bit;
 
+void Reset_lcd(){
+       memset(user_password, 0, strlen(user_password));
+       Lcd_Cmd(_LCD_CLEAR);
+       Lcd_Out(1, 1, "Enter Password:");
+       pass_index = 0;
+       Star_index = 1;
+ }
 
-// LED pin
-sbit LED_Pin at RB6_bit;
-sbit LED_Direction at TRISB6_bit;
-
- //timer
-  int time,counter=0;
-
-
-
-void initialize_timer_Interrupt(void){
-    TMR0IE_bit=1;       // Enable timer 0 interrupt
-    GIE_bit=1;          //Enable Global Interrupt
-    T0CS_bit=0;                  // Select f/4 clock for the TMR0
-    PSA_bit=0;                 // Prescaler is assigned to the Timer0 module
-    PS0_bit=0;                // Set pre-scaler to 32
-    PS1_bit=1;                // PS2,PS1,PS0 = 100
-    PS2_bit=0;
-    TMR0=6;                  //counter starting value
+void initialize_timer_Interrupt(void)
+{
+    TMR0IE_bit = 1; // Enable timer 0 interrupt
+    GIE_bit = 1;    // Enable Global Interrupt
+    T0CS_bit = 0;   // Select f/4 clock for the TMR0
+    PSA_bit = 0;    // Prescaler is assigned to the Timer0 module
+    PS0_bit = 0;    // Set pre-scaler to 8
+    PS1_bit = 1;    // PS2,PS1,PS0 = 010
+    PS2_bit = 0;
+    TMR0 = 6; // counter starting value
 }
 
-void main() {
-  Motor_Direction = 0;  // Set MOTOR pin as output
-  LED_Direction = 0;  // Set LED pin as output
-  TRISB.B1=0;
-  PORTB.B1=0;
-  Keypad_Init();
-  Lcd_Init();
-  Lcd_Cmd(_LCD_CLEAR);
-  Lcd_Cmd(_LCD_CURSOR_OFF);
-  Lcd_Out(1, 1, "Enter Pass:");
+void main()
+{
+    // Init
+    Motor_Direction = 0; // Set MOTOR pin as output
+    Keypad_Init();
+    Lcd_Init();
+    initialize_timer_Interrupt(); // invoke timer interrupt initialization function
+    Lcd_Cmd(_LCD_CLEAR);
+    Lcd_Cmd(_LCD_CURSOR_OFF);
+    Lcd_Out(1, 1, "Enter Password:");
+    
+    while (1)
+    {
+        if (counter == 500)
+        {
+            // check if the counter reaches 500
+            counter = 0;
+            time++;
+            if(motor_running){
+                 IntToStrWithZeros(time,txt);
+                 Lcd_Out(2,6,txt);
+              if(time == 4){
+                     Motor_Pin = 0; // Turn Off the Motor
+                     motor_running = 0;
+                     Delay_ms(1000);
+                     Reset_lcd();
+                    }
 
-while(1){
-           LED_Pin = 1;  // Turn on the LED
-           Delay_ms(100);  // Delay for 1 seconds
-           LED_Pin = 0;  // Turn on the LED
-           Delay_ms(100);  // Delay for 1 seconds
-if(counter>=3000){  // check if the counter reaches 125
-                   Motor_Pin = 0;  // Turn ff the Motor
+            }
 
-            //time=time++;    // increase time one second
-           }
-           
-
-    kp = 0;
-    //do
-      kp = Keypad_Key_Click();             // Store key code in kp variable
-    //while (!kp);
- if (kp && Motor_Pin==0) {
-
-      switch (kp) {
-      case  1: kp = '1'; break; // 1        // Uncomment this block for keypad4x4
-      case  2: kp = '2'; break; // 2
-      case  3: kp = '3'; break; // 3
-      case  5: kp = '4'; break; // 4
-      case  6: kp = '5'; break; // 5
-      case  7: kp = '6'; break; // 6
-      case  9: kp = '7'; break; // 7
-      case 10: kp = '8'; break; // 8
-      case 11: kp = '9'; break; // 9
-      case 13: kp = '*'; break; // *
-      case 14: kp = '0'; break; // 0
-      case 15: kp = '#'; break; // #
-  }
-
-if(kp == '*')
-   {    //clear
-   }
-else if (kp == '#'){
-       for( i=0 ; i <4 ; i++){
-         if(password[i] != My_pass[i]){
-                   Lcd_Cmd(_LCD_CLEAR);
-                   LCD_Out_CP("WRONG");
-                   flag = 0;
-                   break;
-         }
         }
-        if(flag)
-         {         Lcd_Cmd(_LCD_CLEAR);
-                   LCD_Out_CP("CORRECT");
-                   Motor_Pin = 1;  // Turn on the Motor
-            counter=0;      // start counter from the beginning
-                   initialize_timer_Interrupt();   // invoke timer interrupt initialization function
-                   //Motor_Pin = 0;  // Turn off the Motor
-         }
-}else{
-  password[count] = kp;
-  count++;
-  Lcd_Chr_CP('*');
-    }
-      }
-  }
-}
+        kp = 0; // Reset key code variable
+        if(motor_running == 0){
+           kp = Keypad_Key_Click(); // Store key code in kp variable
+        }
+        if (kp != 0)
+        {
+            switch (kp)
+            {
+            case 1:    kp = '1';    break; // 1
+            case 2:    kp = '2';    break; // 2
+            case 3:    kp = '3';    break; // 3
+            case 5:    kp = '4';    break; // 4
+            case 6:    kp = '5';    break; // 5
+            case 7:    kp = '6';    break; // 6
+            case 9:    kp = '7';    break; // 7
+            case 10:   kp = '8';    break; // 8
+            case 11:   kp = '9';    break; // 9
+            case 13:   kp = '*';    break; // *
+            case 14:   kp = '0';    break; // 0
+            case 15:   kp = '#';    break; // #
+            }
+            if (kp == '*')
+            {
+                Reset_lcd();
+            }
+            else if (kp == '#')    //Enter
+            {
+                if (strcmp(My_pass, user_password) == 0)
+                {
+                    Lcd_Cmd(_LCD_CLEAR);
+                    LCD_Out_CP("CORRECT");
+                    time =0;
+                    IntToStrWithZeros(time,txt);
+                    Lcd_Out(2,6,txt);
+                    Motor_Pin = 1; // Turn on the Motor
+                    counter = 0;   // start counter from the beginning
+                    motor_running =1;
 
-void interrupt() {        // Interrupt handler
-  if (INTCON.TMR0IF==1) {     // check for timer 0 interrupt flag
-    counter++;                // increment 1 every interrupt
-    INTCON.TMR0IF=0;          // reset the TMR0IF flag
-    TMR0=6;                   // store 6 in the TMR0 register
-  }
+                }
+                else
+                {
+                    Lcd_Cmd(_LCD_CLEAR);
+                    LCD_Out_CP("WRONG");
+                    Delay_ms(1000);
+                    Reset_lcd();
+                }
+
+            }
+            else
+            {
+                user_password[pass_index] = kp;
+                pass_index++;
+                Lcd_Chr(2, Star_index++, '*');
+                
+            }
+
+        } // First If Closed
+    } // While Close
+} // Main Close
+
+
+// Interrupt handler
+void interrupt()
+{
+    // check for timer 0 interrupt flag
+    if (INTCON.TMR0IF == 1)
+    {
+        counter++;         // increment 1 every interrupt
+        INTCON.TMR0IF = 0; // reset the TMR0IF flag
+        TMR0 = 6;          // store 6 in the TMR0 register
+    }
 }
--- README.md
+++ README.md
@@ -19,16 +19,28 @@ git clone https://github.com/moekhodry11/PIC-Interrupt-Lab.git
 This repository contains several example programs that demonstrate different types of interrupts in the PIC16F877A microcontroller. Each program is located in a separate folder and includes a README file that explains how it works.
 
 The following programs are included:
-
 -External_Interrupt: Demonstrates how to use an external interrupt to detect a button press and toggle an LED.
 
+# Circuit Diagram 
+![photo_2023-05-15_20-47-07](https://github.com/alaawahba13/Garage-lock/assets/101985923/6c8bbfe5-74c2-4eea-ac70-2ff0dcfbb766)
+
+
+# Simulation 
+https://github.com/alaawahba13/Garage-lock/assets/101985923/ca4aafa2-d9e1-497d-a083-309d5cdf9e14
+
+
+
 # Authors
+This code was developed by 
+- [@Alaa Wahba](https://github.com/alaawahba13)
 - [@Ahmed Atef](https://github.com/AhmedAtef283)
 - [@Mohamed Abdullhaleam](https://github.com/Mohamedabdullhaleam)
 - [@Fareda Elsayed]( https://github.com/FaredaElsayed)
 - [@Gehad Alaa ](https://github.com/Gehad799)
 - [@Roaa Gawish ](https://github.com/roaagawish)
-- [@Alaa Wahba](https://github.com/alaawahba13)
 - [@Mohamed Khodary](https://github.com/moekhodry11)
+- [@Mohamed Abdelzaher](https://github.com/Mohamed991-1) 
+- [@Hagar Tarek](https://github.com/Hager706)
+- [@Islam Morgan](https://github.com/retrogradex)
 
 
